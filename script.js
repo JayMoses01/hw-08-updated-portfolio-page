c350cab7590c83e8a868d1c0736673c3de1fd3a4
Jay Moses <jayrmoses@gmail.com>
2022-02-08 00:24:50 -0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// "Burger" menu for mobile
+const menuBurger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+menuBurger.addEventListener('click', () => {
+    navLinks.classList.toggle('is-active');
+});
