34dea12ca58b58f8cce4b8b10b1dc2b3f42f85bd
Jay Moses <jayrmoses@gmail.com>
2022-02-08 22:42:00 -0800

    Adding updated script.js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// "Burger" menu for mobile
+// "Burger" menu for mobile. I referred to "The Net Ninja" Bulma YouTube tutorial on navbar "burgers" here: https://www.youtube.com/watch?v=qvn2SxGvyPs&list=PL4cUxeGkcC9iXItWKbaQxcyDT1u6E7a8a&index=6&t=174s.
 const menuBurger = document.getElementById('burger');
 const navLinks = document.getElementById('nav-links');
 
